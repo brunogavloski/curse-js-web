6a516e13c4e1d8261fc65aa036a9dd85c9a82b7e
Bruno Gavloski <gavloski11@gmail.com>
2021-05-25 17:40:47 -0300

    8.4-2

---
+++ 1/sketch.js
@@ -0,0 +1,19 @@
+
+var bgcolor;
+
+
+function setup() {
+  canvas = createCanvas(200, 200);
+  bgcolor = color(200);
+}
+
+function mousePressed() {
+  bgcolor = color(random(255));
+}
+
+function draw() {
+  background(bgcolor);
+  fill(255, 0, 175);
+  rect(100, 100, 50, 50);
+}
+
