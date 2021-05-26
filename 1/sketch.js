71f8c8921403b99129ac82e02aad048be0fade3b
Bruno Gavloski <gavloski11@gmail.com>
2021-05-26 15:41:24 -0300

    3

--- 1/sketch.js
+++ 1/sketch.js
@@ -1,10 +1,18 @@
-
+var button;
 var bgcolor;
+var slider;
+var input;
 
 
 function setup() {
   canvas = createCanvas(200, 200);
   bgcolor = color(200);
+  createP('you are name here' +  input.value());
+  button = createButton("press");
+  
+  slider = createSlider(1, 100, 50);
+  input = createInput("tell a mesage");
+  
 }
 
 function mousePressed() {
@@ -14,6 +22,8 @@ function mousePressed() {
 function draw() {
   background(bgcolor);
   fill(255, 0, 175);
-  rect(100, 100, 50, 50);
+  ellipse(100, 100, slider.value(), slider.value());
+
+  text(input.value(), 20,20);
 }
 
